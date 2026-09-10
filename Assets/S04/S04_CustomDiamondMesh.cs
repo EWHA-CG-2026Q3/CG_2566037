987d7e6b2046d1058eb6d9dcf7c8a2ebabc248cf
tniauq <bokdong08@gmail.com>
2026-09-10 10:30:06 +0900

    S04 task 1

--- Assets/S04/S04_CustomDiamondMesh.cs
+++ Assets/S04/S04_CustomDiamondMesh.cs
@@ -21,8 +21,13 @@ public class S04_CustomDiamondMesh : MonoBehaviour
         int[] triangles = new int[]
         {
             // 위쪽 4면 (정점 4 사용)
+            4, 0, 1,
+            4, 1, 2,
+            4, 2, 3,
+            4, 0, 3
 
             // 아래쪽 4면 (정점 5 사용)
+            
         };
 
         Mesh mesh = new Mesh();
